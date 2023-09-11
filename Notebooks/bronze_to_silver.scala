e983f2b12e644ea99155ed3d008603f589a73e63
brnodata <brnodata>
2023-09-11 17:20:42 +0000

    Adicionando comentários no código

--- Notebooks/bronze_to_silver.scala
+++ Notebooks/bronze_to_silver.scala
@@ -1,4 +1,10 @@
 // Databricks notebook source
+// MAGIC %md
+// MAGIC
+// MAGIC ## Lendo arquivo delta da camada bronze
+
+// COMMAND ----------
+
 val url = "/mnt/dados/bronze/dataset_imoveis/"
 val df = spark.read.format("delta").load(url)
 
@@ -6,6 +12,12 @@ val df = spark.read.format("delta").load(url)
 // COMMAND ----------
 
 display(df)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC
+// MAGIC # lendo o campo"Anuncio" que está como tipo objeto e separando cada campo em uma coluna
 
 // COMMAND ----------
 
@@ -19,6 +31,12 @@ display(
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC
+// MAGIC ## Salvando os campos separados em um novo Dataframe
+
+// COMMAND ----------
+
 val dados_detalhados = df.select("anuncio.*","anuncio.endereco.*") 
 
 // COMMAND ----------
@@ -27,8 +45,20 @@ display(dados_detalhados)
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC
+// MAGIC ## Dropando colunas que não queremos que façam parte do dados que vão para camada Silver
+
+// COMMAND ----------
+
 val df_silver = dados_detalhados.drop("caracteristicas","endereco")
 display(df_silver)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC
+// MAGIC ## Salvando o dataframe final na camada Silver em formato delta.
 
 // COMMAND ----------
 
